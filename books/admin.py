99e785bf4807cd41628d3252b4c9eed3c3175c32
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2024-11-14 00:34:05 +0300

    Books app: models.py, admin.py, serializers.py, urls.py were created.

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from books.models import Author, Genre, Book, Rent
 
 
@@ -10,20 +9,23 @@ class AuthorAdmin(admin.ModelAdmin):
     list_filter = ('id', 'full_name', 'birth_date', )
     search_fields = ('full_name', 'full_name', 'birth_date', 'death_date', )
 
+
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', )
     list_filter = ('id', 'name', )
     search_fields = ('id', 'name', )
 
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'publication_book_year' ,)
-    list_filter = ('id', 'title', 'author', 'publication_book_year' ,)
+    list_display = ('id', 'title', 'author', 'publication_book_year',)
+    list_filter = ('id', 'title', 'author', 'publication_book_year',)
     search_fields = ('id', 'title', 'author', 'publication_book_year', 'genre', )
+
 
 @admin.register(Rent)
 class RentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'book__title', 'user__username' )
+    list_display = ('id', 'book__title', 'user__username', )
     list_filter = ('id', 'is_book_returned', 'transaction_date_created', 'term', 'deadline', )
     search_fields = ('id', 'title', 'author', 'publication_book_year', 'genre', 'term', 'deadline', )

1de1488fc789a352918ebb8cf90d4fc77394c386
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2024-11-14 00:29:18 +0300

    Books app: models.py, admin.py, serializers.py, urls.py were created.

--- books/urls.py
+++ books/urls.py
@@ -5,13 +5,16 @@ from rest_framework.routers import SimpleRouter
 
 app_name = BooksConfig.name
 
-router = SimpleRouter()
-router.register(r'authors', views.AuthorViewSet, basename='author')
-router.register(r'genres', views.GenreViewSet, basename='genre')
-router.register(r'', views.BookViewSet, basename='book')
-router.register(r'rent', views.RentViewSet, basename='rent')
+router1 = SimpleRouter()
+router1.register(r'authors', views.AuthorViewSet, basename='author')
+router2 = SimpleRouter()
+router2.register(r'genres', views.GenreViewSet, basename='genre')
+router3 = SimpleRouter()
+router3.register(r'', views.BookViewSet, basename='book')
+router4 = SimpleRouter()
+router4.register(r'rent', views.RentViewSet, basename='rent')
 
 urlpatterns = [
 
-] + router.urls
+] + router1.urls + router2.urls + router3.urls + router4.urls
 

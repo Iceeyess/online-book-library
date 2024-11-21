8e3807656ac5d6153cc2a7291cc1172a1a098fe0
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2024-11-21 22:57:00 +0300

    test.py for books app was a bit modified for shorten view and DRY.

--- books/tests.py
+++ books/tests.py
@@ -1,5 +1,6 @@
 import json
 
+from rest_framework.exceptions import APIException
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from django.urls import reverse
@@ -13,17 +14,16 @@ class AuthorGenreBookRentTestCase(APITestCase):
     def get_http_response_for_my_tests(self, method, path, headers1, headers2, expected_status_1,
                                        expected_status_2, data={}, format='json'):
         try:
-            print(f'Обрабатываю через block try для {data}')
+            print(f'Обрабатываю через block try для {data if data else dict(data="no data found")}')
             answer = method(path=path, data=data, format='json', headers=headers1)
             self.assertEqual(answer.status_code, expected_status_1)
-        except:
+        except AssertionError:
             raise ValueError('Unexpected status code.')
         else:
             answer = method(path=path, data=data, format='json', headers=headers2)
-            print(f'Обрабатываю через block else для {data}')
+            print(f'Обрабатываю через block else для {data if data else dict(data="no data found")}')
             self.assertEqual(answer.status_code, expected_status_2)
         return answer
-
 
     def setUp(self) -> None:
         # Superuser and simple user creation
@@ -52,18 +52,18 @@ class AuthorGenreBookRentTestCase(APITestCase):
         }
         self.genre = Genre.objects.create(name='Роман')
         self.author_dict_data = dict(full_name='Михаил Афанасьевич Булгаков', birth_date='19400310',
-                                biography="Русский писатель советского периода, врач, драматург, театральный "
-                                          "режиссёр и актёр. Родился 3 (15)")
+                                     biography="Русский писатель советского периода, врач, драматург, театральный "
+                                               "режиссёр и актёр. Родился 3 (15)")
         self.author = Author.objects.create(**self.author_dict_data)
         self.book_data = dict(title='Мастер и Маргарита', publication_date='19670714',
-                         description='В книге рассказывается о том, как Мастер и Маргарита, две известные истории, '
-                                     'встречаются в самом сердце Канады. Мастер отправляется на поиски Маргариты, '
-                                     'которая была нашей героиней в прошлом.',
-                         genre=[self.genre.id],
-                         author=[self.author.id],
-                         retail_amount = 100,
-                         term=3,
-                         publication_book_year=1967)
+                              description='В книге рассказывается о том, как Мастер и Маргарита, две известные истории,'
+                                          'встречаются в самом сердце Канады. Мастер отправляется на поиски Маргариты, '
+                                          'которая была нашей героиней в прошлом.',
+                              genre=[self.genre.id],
+                              author=[self.author.id],
+                              retail_amount=100,
+                              term=3,
+                              publication_book_year=1967)
 
     def test_crud_genre(self):
         """Testing for CRUD of Genre model"""
@@ -71,7 +71,8 @@ class AuthorGenreBookRentTestCase(APITestCase):
         data = [dict(name='Фантастика'), dict(name='Новелла'), dict(name='Сага')]
         for genre in data:
             self.get_http_response_for_my_tests(self.client.post, path, self.simple_user_headers,
-                                           self.super_user_headers, status.HTTP_403_FORBIDDEN, status.HTTP_201_CREATED,
+                                                self.super_user_headers, status.HTTP_403_FORBIDDEN,
+                                                status.HTTP_201_CREATED,
                                                 data=genre)
 
         ################################################################
@@ -80,7 +81,7 @@ class AuthorGenreBookRentTestCase(APITestCase):
         path_update = reverse('lib:genres-detail', kwargs={'pk': genre.id})
         data_update = dict(name='Классика')
         self.get_http_response_for_my_tests(self.client.patch, path_update, self.simple_user_headers,
-                                           self.super_user_headers, status.HTTP_403_FORBIDDEN, status.HTTP_200_OK,
+                                            self.super_user_headers, status.HTTP_403_FORBIDDEN, status.HTTP_200_OK,
                                             data=data_update)
         ################################################################
         # Test get genre
@@ -138,28 +139,24 @@ class AuthorGenreBookRentTestCase(APITestCase):
         path_update = reverse('lib:books-detail', kwargs={'pk': book.id})
         data_update = dict(title='Звездные войны')
         response_update = self.get_http_response_for_my_tests(self.client.patch, path_update, self.simple_user_headers,
-                                            self.super_user_headers, status.HTTP_403_FORBIDDEN, status.HTTP_200_OK,
-                                            data=data_update)
+                                                              self.super_user_headers, status.HTTP_403_FORBIDDEN,
+                                                              status.HTTP_200_OK,
+                                                              data=data_update)
         self.assertEqual(json.loads(response_update.content)['title'], data_update.get('title'))
         ################################################################################################
         # Test get book
         path_get = reverse('lib:books-list')
-        try:
-            response_get = self.get_http_response_for_my_tests(self.client.get, path_get, self.simple_user_headers,
-                                                                self.super_user_headers, status.HTTP_403_FORBIDDEN,
-                                                                status.HTTP_200_OK)
-        except:
-            print(f'Считываю через block try для списка объекта {Book.__name__}')
-            response_get = self.client.get(path=path_get, format='json', headers=self.simple_user_headers)
-        finally:
-            self.assertEqual(response_get.status_code, status.HTTP_200_OK)
+        response_get = self.get_http_response_for_my_tests(self.client.get, path_get, self.simple_user_headers,
+                                                               self.simple_user_headers, status.HTTP_200_OK,
+                                                               status.HTTP_200_OK)
+        self.assertEqual(response_get.status_code, status.HTTP_200_OK)
         ################################################################################################
         # Test delete book
         book_delete = Book.objects.all().last()
         path_delete = reverse('lib:books-detail', kwargs={'pk': book_delete.id})
         self.get_http_response_for_my_tests(self.client.delete, path_delete, self.simple_user_headers,
-                                                           self.super_user_headers, status.HTTP_403_FORBIDDEN,
-                                                           status.HTTP_204_NO_CONTENT)
+                                            self.super_user_headers, status.HTTP_403_FORBIDDEN,
+                                            status.HTTP_204_NO_CONTENT)
 
     def test_crud_rent(self):
         """Testing for CRUD of Rent model"""
@@ -185,7 +182,7 @@ class AuthorGenreBookRentTestCase(APITestCase):
             response_get = self.get_http_response_for_my_tests(self.client.get, path_get, self.simple_user_headers,
                                                                self.super_user_headers, status.HTTP_403_FORBIDDEN,
                                                                status.HTTP_200_OK)
-        except:
+        except AssertionError:
             print(f'Считываю через block try для списка объекта {Rent.__name__}')
             response_get = self.client.get(path=path_get, format='json', headers=self.super_user_headers)
         finally:
@@ -196,7 +193,7 @@ class AuthorGenreBookRentTestCase(APITestCase):
         try:
             print(f'Возвращаю книгу назад на полку через block try для списка объекта {Rent}')
             response = self.client.patch(path=rent_return_path, format='json', headers=self.simple_user_headers)
-        except:
+        except AssertionError:
             raise ValueError('Unexpected status code.')
         else:
             print(f'Не получилось сделать изначально, поскольку пользователь являлся не создавший запись или суперюзер.'
@@ -212,5 +209,5 @@ class AuthorGenreBookRentTestCase(APITestCase):
         rent_delete = Rent.objects.all().last()
         path_delete = reverse('lib:rent-detail', kwargs={'pk': rent_delete.id})
         self.get_http_response_for_my_tests(self.client.delete, path_delete, self.simple_user_headers,
-                                                           self.super_user_headers, status.HTTP_403_FORBIDDEN,
-                                                           status.HTTP_204_NO_CONTENT)
+                                            self.super_user_headers, status.HTTP_403_FORBIDDEN,
+                                            status.HTTP_204_NO_CONTENT)
